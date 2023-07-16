ffb862d9d429b3e2142ed688f3242d39651a3f25
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-07-16 17:20:02 +0900

    feat: 캐싱 기능 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,20 +1,50 @@
 import axios from 'axios';
 
+type CacheData = {
+  expirationTime: number;
+  data: string[];
+};
+
+// Cache object
+const cache: Record<string, CacheData> = {};
+
+// Life time for cache, example: 5 minutes (in milliseconds)
+const cacheLifetime = 5 * 60 * 1000;
+
 const api = axios.create({
   baseURL: 'http://localhost:4000',
 });
 
 const searchData = async (query: string): Promise<string[]> => {
   console.info('calling api');
+
+  console.log('Cache contents: ', cache);
+  // Check cache
+  if (query in cache && cache[query].expirationTime > Date.now()) {
+    console.info('Returning data from cache');
+
+    return cache[query].data;
+  }
+
   const response = await api.get('/sick', {
     params: { q: query },
   });
 
+  let suggestions: string[];
+
   if (response.data.length > 0) {
-    return response.data.map((item: { sickCd: string; sickNm: string }) => item.sickNm);
+    suggestions = response.data.map((item: { sickCd: string; sickNm: string }) => item.sickNm);
   } else {
-    return ['검색어 없음'];
+    suggestions = ['검색어 없음'];
   }
+
+  // Save the suggestions to cache
+  cache[query] = {
+    expirationTime: Date.now() + cacheLifetime,
+    data: suggestions,
+  };
+
+  return suggestions;
 };
 
 export default searchData;
