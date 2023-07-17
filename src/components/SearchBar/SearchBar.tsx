b39ef7510e947caccd23e04d65d1dcb756962dc1
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-07-17 15:20:09 +0900

    feat: SearchBar 컴포넌트에서 keydown 이벤트 등록해서 추천 검색어들오 이동 가능하도록 구현

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,22 +1,65 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react';
 
 import { useDataContext } from '@/context/DataContext';
 
 const SearchBar = () => {
   const { suggestions, query, setQuery } = useDataContext();
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const suggestionContainerRef = useRef<HTMLUListElement>(null);
+  const suggestionItemRefs = useRef<any>([]);
+
+  useEffect(() => {
+    if (suggestionContainerRef.current && suggestionItemRefs.current[selectedIndex]) {
+      suggestionItemRefs.current[selectedIndex].scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'start',
+      });
+    }
+  }, [selectedIndex]);
 
   const onChange = async (e: ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
   };
 
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setQuery(suggestions[selectedIndex] || query);
+  };
+
+  const keyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'ArrowUp') {
+      setSelectedIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : suggestions.length - 1));
+    } else if (e.key === 'ArrowDown') {
+      setSelectedIndex((prevIndex) => (prevIndex < suggestions.length - 1 ? prevIndex + 1 : 0));
+    } else if (e.key === 'Enter') {
+      e.preventDefault();
+      setQuery(suggestions[selectedIndex] || query);
+    }
+  };
+
   return (
     <>
-      <input type="text" value={query} onChange={onChange} placeholder="질환명 검색" />
-      <ul>
-        {suggestions.map((suggestion, index) => (
-          <li key={index}>{suggestion}</li>
-        ))}
-      </ul>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={query}
+          onChange={onChange}
+          onKeyDown={keyDownHandler}
+          placeholder="질환명 검색"
+        />
+        <ul ref={suggestionContainerRef}>
+          {suggestions.map((suggestion, index) => (
+            <li
+              key={index}
+              ref={(element) => (suggestionItemRefs.current[index] = element)}
+              style={{ backgroundColor: index === selectedIndex ? '#c0c0c0' : 'transparent' }}
+            >
+              {suggestion}
+            </li>
+          ))}
+        </ul>
+      </form>
     </>
   );
 };
